74bf1973c72e3bd841e473c142193c41a7d1d905
godokan <nonamekoo@naver.com>
2023-06-04 21:12:03 +0900

    프로젝트 시작

---
+++ app/src/main/java/com/godokan/rutiner/MainActivity.java
@@ -0,0 +1,14 @@
+package com.godokan.rutiner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
