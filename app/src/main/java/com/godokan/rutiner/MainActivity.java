5e7cc451bad2d24bdb385e955959a630f8ef6820
godokan <nonamekoo@naver.com>
2023-06-05 01:55:23 +0900

    날짜 구현 첫 단계

--- app/src/main/java/com/godokan/rutiner/MainActivity.java
+++ app/src/main/java/com/godokan/rutiner/MainActivity.java
@@ -1,14 +1,35 @@
 package com.godokan.rutiner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.time.LocalDate;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    Calendar cal;
+    RecyclerView calendarView;
+    LocalDate selectedDate;
+    CalendarAdapter calendarAdapter;
+    Button btnPrev, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+
+    }
+
+    private void setCalendar() {
+
+        calendarView = findViewById(R.id.recyclerView);
+        btnPrev = calendarView.findViewById(R.id.btnPrev);
+        btnNext = calendarView.findViewById(R.id.btnNext);
+        selectedDate = LocalDate.now();
+
     }
 }
