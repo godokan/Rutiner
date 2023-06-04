bb80a509137add4090f1114b2216d2c1baa87e9b
godokan <nonamekoo@naver.com>
2023-06-05 02:57:09 +0900

    누락 파일 수정

---
+++ app/src/main/java/com/godokan/rutiner/CalendarAdapter.java
@@ -0,0 +1,46 @@
+package com.godokan.rutiner;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder> {
+    class CalendarViewHolder extends RecyclerView.ViewHolder {
+        TextView dayView;
+
+        public CalendarViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dayView = itemView.findViewById(R.id.tvDay);
+        }
+    }
+
+    ArrayList<String> days;
+
+    public CalendarAdapter(ArrayList<String> days) {
+        this.days = days;
+    }
+
+    @NonNull
+    @Override
+    public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.calendar_cell, parent, false);
+        return new CalendarViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position) {
+        holder.dayView.setText(days.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return days.size();
+    }
+}
