395dde3791bdee9520a0cb0343133b622eb70107
Godokan <nonamekoo@naver.com>
2023-06-05 15:12:35 +0900

    달 선택 기능 추가

--- app/src/main/java/com/godokan/rutiner/MainActivity.java
+++ app/src/main/java/com/godokan/rutiner/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     LocalDate selectedDate;
     CalendarAdapter calendarAdapter;
     Button btnPrev, btnNext;
+    TextView tvMonth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +30,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private String monthSelecter(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
+        return date.format(formatter);
+    }
+
     private void setCalendar() {
 
         calendarView = findViewById(R.id.recyclerView);
         btnPrev = calendarView.findViewById(R.id.btnPrev);
         btnNext = calendarView.findViewById(R.id.btnNext);
+        tvMonth = findViewById(R.id.tvMonth);
         selectedDate = LocalDate.now();
+
+    }
+
+    private void setMonth() {
 
     }
 }
